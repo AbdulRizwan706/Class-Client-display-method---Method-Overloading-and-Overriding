13654bdc3dc2221e0125bec0df9d99fe05029b8e
Abdul Rizwan <abdulrizwan706@gmail.com>
2023-04-26 18:55:27 +0530

    first commit

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,10 @@
 import java.util.Stack;
-
+// We can pass all data-types that supports looping over its data
+// Write the code to print individual items of the passed data over separate lines
+// Syntax requirements on display method
+// display method should be static
+// display method’s return type should be void
+// display method will take one parameter of appropriate type that allows it to loop over data
 public class Client {
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
@@ -8,9 +13,16 @@ public class Client {
         st.push(30);
 
         // Make a call to display method as per question requirements here
+        display(st);
 
     }
 
     // Write the display method as per requirements of question here
+    public static void display(Iterable<?> data) {
+        for (Object item : data) {
+            System.out.println(item);
+        }
+    }
+    
 
 }
